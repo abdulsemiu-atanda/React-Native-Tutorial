b4cf400adcbe4914115473f08355478a326dca06
Abdulsemiu Atanda <semiu@live.com>
2017-06-13 22:34:26 +0100

    remove debugging code and add search component

--- components/userInput.js
+++ components/userInput.js
@@ -1,19 +1,54 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { Alert, View, Button, TextInput, StyleSheet } from "react-native";
 
 class UserInput extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      text: ""
+    }
+    this.onButtonPress = this.onButtonPress.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
+  onChange(text) {
+    this.text = text
+  }
+  onButtonPress(text) {
+    Alert.alert(`You wanna search twitter for ${this.text}`);
+  }
   render() {
     return (
-      <View>
-        <Text style={styles.text}>This will take inputs</Text>
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          onChangeText={this.onChange}
+        />
+        <Button
+          onPress={this.onButtonPress}
+          title="Search"
+          style={{ color: "purple" }}
+          accessibilityLabel="Dispatches action that searches twitter for input"
+        />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  text: {
-    paddingTop: 40
+  container: {
+    flex: 1,
+    justifyContent: "flex-start",
+    paddingTop: 150,
+    backgroundColor: "#F7F7F7"
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#D7D7D7",
+    height: 50,
+    marginRight: 10,
+    marginLeft: 10,
+    padding: 15,
+    borderRadius: 3,
   }
 });
 
--- reducers/twitterTrendsReducer.js
+++ reducers/twitterTrendsReducer.js
@@ -1,6 +1,5 @@
 let initialState = ["obasanjo", "atiku", "UCL"]
 export default (state = initialState, action) => {
-  console.log(action);
   switch (action.type) {
     case "TRENDS_FETCHED": {
       return initialState = action.payload;
